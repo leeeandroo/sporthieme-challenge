ef9ab401f17e47a6da9ae5b686118ce4f7130646
Leandro Vasco da Rocha <leeeandroo@gmail.com>
2020-02-14 20:13:17 +0100

    feat(github): Check if token exist or ask for authorize

--- sport-thieme-gh/src/components/github/index.js
+++ sport-thieme-gh/src/components/github/index.js
@@ -3,11 +3,24 @@ import React from 'react';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 
+import Login from '../auth/login';
+
 function Github() {
+  const [token] = React.useState(localStorage.getItem('github_token'));
+
   return (
     <Grid item p={2}>
       <Box component="div" p={2}>
-        <p>Please choose the repository you want to see more information:</p>
+        {token ? (
+          <p>Please choose the repository you want to see more information:</p>
+        ) : (
+          <React.Fragment>
+            <p>
+              Please authorize our app to allow us to see your repositories.
+            </p>
+            <Login />
+          </React.Fragment>
+        )}
       </Box>
     </Grid>
   );
