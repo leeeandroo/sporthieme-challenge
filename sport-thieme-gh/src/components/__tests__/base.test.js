c8e56d715958c3c2966dc04994e9a613cc29fe8e
Leandro Vasco da Rocha <leeeandroo@gmail.com>
2020-02-14 17:38:08 +0100

    test(base): Test base component

---
+++ sport-thieme-gh/src/components/__tests__/base.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Base from '../base';
+
+describe('Base Component', () => {
+  it('renders the children', () => {
+    const { getByText } = render(<Base>Children component</Base>);
+    expect(getByText(/Children component/i)).toBeInTheDocument();
+  });
+
+  it('renders the Sport-Thieme logo', () => {
+    const { getByAltText } = render(<Base>Children component</Base>);
+    expect(getByAltText('Sport-Thieme')).toBeInTheDocument();
+  });
+
+  it('renders the GitHub authorize button', () => {
+    const { getByText } = render(<Base>Children component</Base>);
+    expect(getByText(/Authorize/i)).toBeInTheDocument();
+  });
+});
