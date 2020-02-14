e21f2ea13e4611cba655fe06652c195b51868800
Leandro Vasco da Rocha <leeeandroo@gmail.com>
2020-02-14 10:13:06 +0100

    test: It renders app component and company logo

--- sport-thieme-gh/src/app.test.js
+++ sport-thieme-gh/src/app.test.js
@@ -2,4 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './app';
 
-test.skip('renders learn react link', () => {});
+it('renders without crashing', () => {
+  const { getByAltText } = render(<App />);
+  expect(getByAltText('Sport-Thieme')).toBeInTheDOM();
+});
