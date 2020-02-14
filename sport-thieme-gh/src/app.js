847402d2d9ac650b33b113f0f553ed4882a73f4a
Leandro Vasco da Rocha <leeeandroo@gmail.com>
2020-02-14 09:31:07 +0100

    refactor: Rename app to lowercase, add company logo and skip test

---
+++ sport-thieme-gh/src/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import logo from './images/sport-thieme-logo.png';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="Sport-thieme" alt="Sport-Thieme" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
